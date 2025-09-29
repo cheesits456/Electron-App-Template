a5d888d222657993857750109999d70e64d7b797
Quin F. <quin@cheesits456.dev>
2025-09-28 18:52:47 -0700

    Create Electron app template

---
+++ index.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+const electron = require("electron");
+
+function createWindow() {
+
+	const window = new electron.BrowserWindow({
+		width: 1200,
+		height: 650,
+		minWidth: 1200,
+		minHeight: 650,
+		maxWidth: 1200,
+		maxHeight: 650,
+		icon: path.join(__dirname, "icon", "icon.png"),
+		webPreferences: {
+			contextIsolation: false,
+			enableRemoteModule: true,
+			nodeIntegration: true
+		}
+	});
+
+	window.removeMenu();
+	window.loadFile(path.join("page", "index.html"));
+}
+
+electron.app.whenReady().then(createWindow);
+
+process.on("uncaughtException", console.error);
